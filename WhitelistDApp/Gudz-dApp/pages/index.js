f0d35f933a212cfc48137af5a7ac1bf5beae08f8
Deelooow <111339711+Deelooow@users.noreply.github.com>
2022-08-28 22:01:54 -0700

    Update index.js

--- WhitelistDApp/Gudz-dApp/pages/index.js
+++ WhitelistDApp/Gudz-dApp/pages/index.js
@@ -192,6 +192,7 @@ export default function Home() {
   return (
     <div>
       <Head>
+      document.getElementById('a').style.backgroundImage="url(/public/bckgrd.png)";
         <title>Gudzlist Dapp</title>
         <meta name="description" content="Gudz-Dapp" />
         <link rel="icon" href="/favicon2.png" />
